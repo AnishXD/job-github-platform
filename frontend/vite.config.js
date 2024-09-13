2c23b368983d5ff0b3ba54287c85ff779fe4cbeb
AnishXD <110338891+AnishXD@users.noreply.github.com>
2024-09-13 23:50:49 +0530

    Update vite.config.js revaluated

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
   },
   build: {
     rollupOptions: {
-      external: ['redux-persist/es/storage'],
+      external: ['redux-persist/lib/storage'],
     },
   },
 });
