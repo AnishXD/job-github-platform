d58dfc2a6da88b1543f6983149a38d17cc293d72
AnishXD <110338891+AnishXD@users.noreply.github.com>
2024-11-07 11:31:04 +0530

    Update HeroSection.jsx thur 7 nov

--- frontend/src/components/HeroSection.jsx
+++ frontend/src/components/HeroSection.jsx
@@ -20,7 +20,10 @@ const HeroSection = () => {
             <div className='flex flex-col gap-5 my-10'>
                 <span className=' mx-auto px-4 py-2 rounded-full bg-gray-100 text-[#F83002] font-medium'>No. 1 Job Hunt Website</span>
                 <h1 className='text-5xl font-bold'>Search, Apply & <br /> Get Your <span className='text-[#6A38C2]'>Dream Jobs</span></h1>
-                <p>A platform for job seekers to apply and recruiters to post and manage openings.</p>
+                <p>A platform for job seekers to apply and recruiters to post and manage openings.<br />
+                    <span className='text-red-500 font-bold'>(Please Login/SignUp to access the website)</span>
+                
+                </p>
                 <div className='flex w-[40%] shadow-lg border border-gray-200 pl-3 rounded-full items-center gap-4 mx-auto'>
                     <input
                         type="text"
